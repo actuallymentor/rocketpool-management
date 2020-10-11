5814f3b795b508ead9fbb8e630cae09a7642ca0c
Mentor Palokaj <mentor@palokaj.co>
2020-10-11 15:04:49 +0200

    logging

--- logresources.source.zsh
+++ logresources.source.zsh
@@ -5,6 +5,7 @@ source ./notify.source.zsh
 function logresources() {
 
 	# All in KiB
+	echo "Getting memory stats"
 	memtotal=$( grep MemTotal /proc/meminfo | grep -Po "[0-9]+" )
 	memfree=$( grep MemFree /proc/meminfo | grep -Po "[0-9]+" )
 	memutil=$(( $memfree * 100 / $memtotal ))
@@ -13,17 +14,22 @@ function logresources() {
 	swaputil=$(( $swapfree * 100 / $swaptotal ))
 
 	# Default log
+	echo "Log resources to log"
 	rplogger "[info] $memutil % / $memtotal KiB RAM, $swaputil % / $swaptotal KiB SWAP"
 
 	# Send the status to the logs and push notification
+	echo "RAM emergency logging"
 	if (( memutil < $RAMWARNINGPERCENT )); then
 		rplogger "[RAM] $memutil % of $(( $memtotal / 1024 / 1024 ))GB"
 		notify "Resource Warning" "RAM usage $memutil percent"
 	fi
 
+	echo "Swap emergency logging"
 	if (( $SWAPWARNINGPERCENT < 50 )); then
 		rplogger "[SWAP] $swaputil % of $(( $swaputil / 1024 / 1024 ))GB"
 		notify "Resource Warning" "SWAP usage $swaputil percent"
 	fi
+
+	echo "Resources log done"
 
 }
