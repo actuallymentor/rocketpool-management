f468f5d64f86d3cd05935c04918105d609862864
Mentor Palokaj <mentor@palokaj.co>
2020-10-11 19:37:28 +0200

    unified logging

--- logresources.source.zsh
+++ logresources.source.zsh
@@ -5,34 +5,33 @@ source ./notify.source.zsh
 function logresources() {
 
 	# All in KiB
+
+	# Memory
 	echo "Getting memory stats"
 	memtotal=$( grep MemTotal /proc/meminfo | grep -Po "[0-9]+" )
 	memfree=$( grep MemFree /proc/meminfo | grep -Po "[0-9]+" )
 	memfreepercent=$(( $memfree * 100 / $memtotal ))
 	memutil=$(( 100 - $memfreepercent ))
 
+	# Swap
 	swaptotal=$( grep SwapTotal /proc/meminfo | grep -Po "[0-9]+" )
 	swapfree=$( grep SwapFree /proc/meminfo | grep -Po "[0-9]+" )
 	swapfreepercent=$(( $swapfree * 100 / $swaptotal ))
 	swaputil=$(( 100 - $swapfreepercent ))
 
+	# Pretty representation
+	memlog="$memutil% RAM util ($memfree/$memtotal), $swaputil% ($swapfree/$swaptotal) SWAP util"
+
 	# Default log
 	echo "Log resources to log"
-	rplogger "[info] $memutil% RAM ($memfree/$memtotal), $swaputil% SWAP ($swapfree/$swaptotal)"
+	
+	rplogger "[info] $memlog"
 
 	# Send the status to the logs and push notification
-	echo "RAM emergency logging"
-	if (( memutil < $RAMWARNINGPERCENT )); then
-		warning="[RAM] Warning: $memutil% utilisation ($memfree/$memtotal), SWAP: $swaputil%"
-		rplogger $warning
-		notify "Rocketpool Warning" $warning
-	fi
-
-	echo "Swap emergency logging"
-	if (( $SWAPWARNINGPERCENT < 50 )); then
-		warning="[SWAP] Warning: $swaputil% utilisation ($swapfree/$swaptotal)"
-		rplogger $warning
-		notify "Resource Warning" $warning
+	echo "Resource emergency logging"
+	if (( $memutil > $RAMWARNINGPERCENT || $swaputil > $SWAPWARNINGPERCENT )); then
+		rplogger $memlog
+		notify "Rocketpool" $memlog
 	fi
 
 	echo "Resources log done"
