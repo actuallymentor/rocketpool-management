ce8488332e40ebbb74fa6a3713c3d8dd8d4468cf
Mentor Palokaj <mentor@palokaj.co>
2020-10-12 14:14:44 +0200

    Add specific eth amount to push

--- notifyIf16EthAvailable.source.zsh
+++ notifyIf16EthAvailable.source.zsh
@@ -7,8 +7,8 @@ function notifyIf16EthAvailable() {
 	ethavailable=$( rocketpool node status | grep -Po "([0-9][0-9])(?=(\.[0-9][0-9] ETH))" )
 
 	if (( ethavailable > 16 )); then
-		rplogger "[notifyIf16EthAvailable] 16 ETH available, sending push"
-		notify "Over 16 ETH in node!" "Go set up a minipool yo"
+		rplogger "[notifyIf16EthAvailable] $ethavailable ETH available, sending push"
+		notify "$ethavailable ETH in node!" "Go set up a minipool yo"
 	fi
 
 	echo "16ETH logger done"
