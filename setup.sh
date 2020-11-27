5c474377ced94764e82e8685370d5d0fd12430bd
Mentor Palokaj <mentor@palokaj.co>
2020-11-27 15:47:40 +0100

    Abstract out devops

--- setup.sh
+++ setup.sh
@@ -1,3 +1,25 @@
+####################
+# Security settings
+####################
+
+# Go Ethereum: https://geth.ethereum.org/docs/interface/private-network#setting-up-networking
+sudo ufw allow 30303:30305/tcp comment 'Go Ethereum'
+sudo ufw allow 30303:30305/udp comment 'Go Ethereum'
+
+# Teku port number is coincidence https://gist.github.com/Larrypcdotcom/fcd4e79c2cf02ce37ec6ed9797beca2c#ports
+sudo ufw allow 9001/tcp comment 'Roketpool arbitrary default port'
+sudo ufw allow 9001/udp comment 'Roketpool arbitrary default port'
+
+# Prysm: https://docs.prylabs.network/docs/prysm-usage/p2p-host-ip/#incoming-p2p-connection-prerequisites
+sudo ufw allow 13000/tcp comment 'Prysm node'
+sudo ufw allow 13000/udp comment 'Prysm node'
+
+# Lighthouse: https://lighthouse-book.sigmaprime.io/advanced_networking.html
+sudo ufw allow 9000/tcp comment 'Lighthouse node'
+sudo ufw allow 9000/udp comment 'Lighthouse node'
+
+sudo ufw enable
+
 ###################
 # Rocketpool setup
 ###################
@@ -43,10 +65,3 @@ rocketpool wallet init
 
 rocketpool node register
 rocketpool node deposit
-
-##################
-# OPTIONAL
-#################
-
-# Optional if you want to automate minipool creation, functions/minipool.16.expect
-apt install -y expect
